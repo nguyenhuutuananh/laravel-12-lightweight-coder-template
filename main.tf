3bb498648b764e7af1b81196c55d994121bc7e06
Tuan Anh Nguyen <tuananh-nguyen@am-bition.vn>
2025-11-03 10:39:24 +0700

    add git, username, workspace directory parameters

--- main.tf
+++ main.tf
@@ -62,6 +62,42 @@ data "coder_parameter" "init_laravel_project" {
   icon         = "https://laravel.com/img/logomark.min.svg"
 }
 
+data "coder_parameter" "workspace_directory" {
+  name         = "Workspace Directory"
+  description  = "Default workspace directory path (relative to home directory)"
+  type         = "string"
+  default      = "workspace"
+  mutable      = false
+  icon         = "https://cdn-icons-png.flaticon.com/512/716/716784.png"
+}
+
+data "coder_parameter" "git_author_name" {
+  name         = "Git Author Name"
+  description  = "Your name for git commits"
+  type         = "string"
+  default      = ""
+  mutable      = true
+  icon         = "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+}
+
+data "coder_parameter" "git_author_email" {
+  name         = "Git Author Email"
+  description  = "Your email for git commits"
+  type         = "string"
+  default      = ""
+  mutable      = true
+  icon         = "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+}
+
+data "coder_parameter" "username" {
+  name         = "Username"
+  description  = "System username for the workspace"
+  type         = "string"
+  default      = "coder"
+  mutable      = false
+  icon         = "https://cdn-icons-png.flaticon.com/512/1077/1077114.png"
+}
+
 resource "coder_agent" "main" {
   os   = "linux"
   arch = "arm64"  # Changed from "amd64"
@@ -69,8 +105,10 @@ resource "coder_agent" "main" {
     set -e
 
     # Create workspace directory
-    mkdir -p ~/workspace
-    cd ~/workspace
+    WORKSPACE_DIR="${data.coder_parameter.workspace_directory.value}"
+    USER_HOME="/home/${data.coder_parameter.username.value}"
+    mkdir -p $USER_HOME/$WORKSPACE_DIR
+    cd $USER_HOME/$WORKSPACE_DIR
 
     # Install dotfiles if provided
     if [ -n "${data.coder_parameter.dotfiles_url.value}" ]; then
@@ -98,7 +136,7 @@ resource "coder_agent" "main" {
         php artisan migrate --force
 
         echo "Laravel project created successfully!"
-        echo "Navigate to ~/workspace/laravel-app to start developing"
+        echo "Navigate to $USER_HOME/$WORKSPACE_DIR/laravel-app to start developing"
         echo "Run 'php artisan serve --host=0.0.0.0 --port=8000' to start the development server"
       else
         echo "Laravel project already exists or init_laravel_project is disabled"
@@ -109,20 +147,20 @@ resource "coder_agent" "main" {
   EOT
 
   env = {
-    GIT_AUTHOR_NAME     = "coder"
-    GIT_COMMITTER_NAME  = "coder"
-    GIT_AUTHOR_EMAIL    = "coder@example.com"
-    GIT_COMMITTER_EMAIL = "coder@example.com"
+    GIT_AUTHOR_NAME     = data.coder_parameter.git_author_name.value != "" ? data.coder_parameter.git_author_name.value : "coder"
+    GIT_COMMITTER_NAME  = data.coder_parameter.git_author_name.value != "" ? data.coder_parameter.git_author_name.value : "coder"
+    GIT_AUTHOR_EMAIL    = data.coder_parameter.git_author_email.value != "" ? data.coder_parameter.git_author_email.value : "coder@example.com"
+    GIT_COMMITTER_EMAIL = data.coder_parameter.git_author_email.value != "" ? data.coder_parameter.git_author_email.value : "coder@example.com"
   }
 
-  dir = "/home/coder"
+  dir = "/home/${data.coder_parameter.username.value}"
 }
 
 resource "coder_app" "code-server" {
   agent_id     = coder_agent.main.id
   slug         = "code-server"
   display_name = "VS Code"
-  url          = "http://localhost:13337?folder=/home/coder/workspace"
+  url          = "http://localhost:13337?folder=/home/${data.coder_parameter.username.value}/${data.coder_parameter.workspace_directory.value}"
   icon         = "/icon/code.svg"
   subdomain    = false
   share        = "owner"
@@ -163,7 +201,7 @@ resource "docker_image" "main" {
     context = "./build"
     dockerfile = "Dockerfile"
     build_args = {
-      USER        = "coder"
+      USER        = data.coder_parameter.username.value
       PHP_VERSION = data.coder_parameter.php_version.value
     }
   }
